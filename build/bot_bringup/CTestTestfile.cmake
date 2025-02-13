e8c5fb8292041deef3d9a3508b30f952a34a91b5
ThepokKung <pok23250@gmail.com>
2025-02-13 16:23:41 +0700

    Delte BIL and add .gitignore

--- build/bot_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kraiwich/Lab1_ws/src/bot_bringup
-# Build directory: /home/kraiwich/Lab1_ws/build/bot_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
