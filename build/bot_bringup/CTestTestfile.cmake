f50996a569bbaf755aad7214c999b4e7047ec8c4
ThepokKung <pok23250@gmail.com>
2025-02-13 16:12:01 +0700

    Download all file to folder

---
+++ build/bot_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kraiwich/Lab1_ws/src/bot_bringup
+# Build directory: /home/kraiwich/Lab1_ws/build/bot_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
